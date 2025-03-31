dc9a0428e8d306413c74472929b7ecbc464270da
Akudinso <nwekekingsley0@gmail.com>
2025-03-31 07:43:58 -0700

    improved bulkupload and modified pdf layout

--- src/routes/payslip.routes.ts
+++ src/routes/payslip.routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { exportPayrollsCSV, exportPayrollsPDF, getPayslipController } from '../controllers/payslip.controller';
+import { exportPayrollsCSV, exportPayrollsPDF, getPayslipController, getPayslipPDFController, getPayslipCSVController } from '../controllers/payslip.controller';
 import { verifyAdmin } from '../middlewares/authMiddleware';
 
 
 const router = express.Router();
+
+
+router.get("/pdf/:payrollId", verifyAdmin, getPayslipPDFController);
+router.get("/csv/:payrollId", verifyAdmin, getPayslipCSVController);
+
 
 router.get('/exportcsv', verifyAdmin, exportPayrollsCSV);
 router.get('/exportpdf', verifyAdmin, exportPayrollsPDF);
